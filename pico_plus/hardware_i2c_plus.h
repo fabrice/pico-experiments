7bcf48af38c9bb0924b26c41848031fd5b47ecb0
fabrice truillot de chambrier <fabrice@dechambrier.net>
2026-03-26 21:05:09 +0100

    pico_plus

---
+++ pico_plus/hardware_i2c_plus.h
@@ -0,0 +1,54 @@
+//----------------------------------------------------------------
+//
+// Raspberry Pico
+// Target : PicoSDK C/C++
+//
+// hardware_i2c_plus
+//
+//----------------------------------------------------------------
+
+#pragma once
+#ifndef _HARDWARE_I2C_PLUS_H
+#define _HARDWARE_I2C_PLUS_H
+
+//----------------------------------------------------------------
+
+#include "hardware/i2c.h"
+
+//----------------------------------------------------------------
+// hardware/i2c
+
+using i2c_ref = i2c_inst_t*;
+
+//----------------------------------------------------------------
+
+inline int i2c_write_bytes_blocking( i2c_inst_t* i2c, uint8_t addr, uint8_t byte1 ) {
+	return i2c_write_blocking( i2c, addr, &byte1, 1, false );
+}
+
+//----------------------------------------------------------------
+
+inline int i2c_write_bytes_blocking( i2c_inst_t* i2c, uint8_t addr, uint8_t byte1, uint8_t byte2 ) {
+	uint8_t buffer[] = { byte1, byte2 };
+	return i2c_write_blocking( i2c, addr, buffer, sizeof(buffer), false );
+}
+
+//----------------------------------------------------------------
+
+inline int i2c_write_bytes_blocking( i2c_inst_t* i2c, uint8_t addr, uint8_t byte1, uint8_t byte2, uint8_t byte3 ) {
+	uint8_t buffer[] = { byte1, byte2, byte3 };
+	return i2c_write_blocking( i2c, addr, buffer, sizeof(buffer), false );
+}
+
+//----------------------------------------------------------------
+
+inline int i2c_write_bytes_blocking( i2c_inst_t* i2c, uint8_t addr, uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4 ) {
+	uint8_t buffer[] = { byte1, byte2, byte3, byte4 };
+	return i2c_write_blocking( i2c, addr, buffer, sizeof(buffer), false );
+}
+
+//----------------------------------------------------------------
+
+#endif
+
+//----------------------------------------------------------------
---
+++ pico_plus/hardware_spi_plus.h
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------
+//
+// Raspberry Pico
+// Target : PicoSDK C/C++
+//
+// hardware_spi_plus
+//
+//----------------------------------------------------------------
+
+#pragma once
+#ifndef _HARDWARE_SPI_PLUS_H
+#define _HARDWARE_SPI_PLUS_H
+
+//----------------------------------------------------------------
+
+#include "hardware/spi.h"
+
+//----------------------------------------------------------------
+// hardware/spi
+
+using spi_ref = spi_inst_t*;
+
+//----------------------------------------------------------------
+
+static inline spi_inst_t* spi_get_instance( uint num ) {
+    invalid_params_if( HARDWARE_SPI, num >= NUM_SPIS );
+	return SPI_INSTANCE( num );
+}
+
+//----------------------------------------------------------------
+
+#endif
+
+//----------------------------------------------------------------
