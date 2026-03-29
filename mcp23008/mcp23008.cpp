45108673d8811b6b11e21d54b960deecda77e82e
fabrice truillot de chambrier <fabrice@dechambrier.net>
2026-03-29 20:43:17 +0200

    mcp23008 up and running

--- mcp23008/mcp23008.cpp
+++ mcp23008/mcp23008.cpp
@@ -29,6 +29,32 @@ constexpr uint8_t MCP23008_OLAT = 0x0a;
 
 //----------------------------------------------------------------
 
+mcp23008::mcp23008():
+	wire( nullptr ),
+	address( MCP23008_ADDRESS ),
+	gpio_dir( 0x00 ),
+	gpio_pull_up( 0x00 ) {
+	this->gpio_init();
+}
+
+//----------------------------------------------------------------
+
+mcp23008::mcp23008( i2c_ref wire, uint8_t address, uint8_t gpio_dir, uint8_t gpio_pull_up ):
+	wire( wire ),
+	address( address ),
+	gpio_dir( ~gpio_dir ),
+	gpio_pull_up( gpio_pull_up ) {
+	this->gpio_init();
+}
+
+//----------------------------------------------------------------
+
+mcp23008::~mcp23008() {
+	this->gpio_put_all( 0 );
+}
+
+//----------------------------------------------------------------
+
 void mcp23008::gpio_init() {
 	uint8_t buffer[] = {
 		MCP23008_IODIR, // register IODIR
@@ -49,30 +75,61 @@ void mcp23008::gpio_init() {
 	i2c_write_bytes_blocking( wire, address, MCP23008_GPPU, gpio_pull_up );
 }
 
+//----------------------------------------------------------------
+
 void mcp23008::gpio_set_dir( uint8_t gpio, bool out ) {
 	if ( gpio > 7 ) return;
+
 	if ( out ) gpio_dir &= ~(1 << gpio);
 	else gpio_dir |= (1 << gpio);
 	i2c_write_bytes_blocking( wire, address, MCP23008_IODIR, gpio_dir );
 }
 
+//----------------------------------------------------------------
+
 void mcp23008::gpio_set_pull_up( uint8_t gpio, bool up ) {
 	if ( gpio > 7 ) return;
+
 	if ( up ) gpio_pull_up |= (1 << gpio);
 	else gpio_pull_up &= ~(1 << gpio);
 	i2c_write_bytes_blocking( wire, address, MCP23008_GPPU, gpio_pull_up );
 }
 
-uint8_t mcp23008::gpio_get_all() {
-	uint8_t value = 0;
-	i2c_write_bytes_blocking( wire, address, MCP23008_GPIO );
-	i2c_read_blocking( wire, address, &value, sizeof( value ), false );
+//----------------------------------------------------------------
 
-	return value;
+uint8_t mcp23008::gpio_get_all() {
+	uint8_t data = 0;
+	i2c_write_bytes_blocking( wire, address, MCP23008_GPIO );
+	i2c_read_blocking( wire, address, &data, sizeof( data ), false );
+
+	return data;
 }
+
+//----------------------------------------------------------------
 
 void mcp23008::gpio_put_all( uint8_t value ) {
 	i2c_write_bytes_blocking( wire, address, MCP23008_GPIO, value );
+}
+
+//----------------------------------------------------------------
+
+uint8_t mcp23008::gpio_get( uint8_t gpio ) {
+	if ( gpio > 7 ) return 0;
+
+	return !!(this->gpio_get_all() >> gpio);
+}
+
+//----------------------------------------------------------------
+
+void mcp23008::gpio_put( uint8_t gpio, bool value ) {
+	if ( gpio > 7 ) return;
+
+	uint8_t data = this->gpio_get_all();
+
+	if ( value ) data |= (1 << gpio);
+	else data &= ~(1 << gpio);
+
+	this->gpio_put_all( data );
 }
 
 //----------------------------------------------------------------
--- mcp23008/mcp23008.h
+++ mcp23008/mcp23008.h
@@ -18,6 +18,11 @@
 
 //----------------------------------------------------------------
 
+class mcp23008;
+using mcp23008_ref = mcp23008*;
+
+//----------------------------------------------------------------
+
 constexpr uint8_t MCP23008_ADDRESS = 0x20;
 
 //----------------------------------------------------------------
@@ -34,16 +39,25 @@ private:
 
 public:
 
-	mcp23008(): wire( nullptr ), address( MCP23008_ADDRESS ), gpio_dir( 0 ), gpio_pull_up( 0 ) {}
-	mcp23008( i2c_ref wire, uint8_t address, uint8_t gpio_dir, uint8_t gpio_pull_up ): wire( wire ), address( address ), gpio_dir( gpio_dir ), gpio_pull_up( gpio_pull_up ) {}
+	mcp23008();
+	mcp23008( i2c_ref wire, uint8_t address, uint8_t gpio_dir, uint8_t gpio_pull_up );
+
+private:
 
 	void gpio_init();
+
+public:
+
+	~mcp23008();
 
 	void gpio_set_dir( uint8_t gpio, bool out );
 	void gpio_set_pull_up( uint8_t gpio, bool up );
 
 	uint8_t gpio_get_all();
 	void gpio_put_all( uint8_t value );
+
+	uint8_t gpio_get( uint8_t gpio );
+	void gpio_put( uint8_t gpio, bool value );
 
 };
 
