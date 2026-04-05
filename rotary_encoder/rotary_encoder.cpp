2d09543e270bd91551b3c4cf862bfe5d69f8eb76
fabrice truillot de chambrier <fabrice@dechambrier.net>
2026-04-05 20:23:57 +0200

    encoder underscore

--- rotary_encoder/rotary_encoder.cpp
+++ rotary_encoder/rotary_encoder.cpp
@@ -26,22 +26,22 @@ constexpr uint ENCODER_S_GPIO = 9;
 //----------------------------------------------------------------
 
 rotary_encoder::rotary_encoder():
-	a_gpio( ENCODER_A_GPIO ),
-	b_gpio( ENCODER_B_GPIO ),
-	s_gpio( ENCODER_S_GPIO ),
-	rotations( 0 ),
-	clicks( 0 ) {
+	_a_gpio( ENCODER_A_GPIO ),
+	_b_gpio( ENCODER_B_GPIO ),
+	_s_gpio( ENCODER_S_GPIO ),
+	_rotations( 0 ),
+	_clicks( 0 ) {
 	this->io_init();
 }
 
 //----------------------------------------------------------------
 
 rotary_encoder::rotary_encoder( uint a_gpio, uint b_gpio, uint s_gpio ):
-	a_gpio( a_gpio ),
-	b_gpio( b_gpio ),
-	s_gpio( s_gpio ),
-	rotations( 0 ),
-	clicks( 0 ) {
+	_a_gpio( a_gpio ),
+	_b_gpio( b_gpio ),
+	_s_gpio( s_gpio ),
+	_rotations( 0 ),
+	_clicks( 0 ) {
 	this->io_init();
 }
 
@@ -54,20 +54,20 @@ rotary_encoder::~rotary_encoder() {
 //----------------------------------------------------------------
 
 void rotary_encoder::io_init() {
-	gpio_init( this->a_gpio );
-    gpio_set_dir( this->a_gpio, GPIO_IN );
-	gpio_disable_pulls( this->a_gpio );
-	gpio_set_input_hysteresis_enabled( this->a_gpio, true );
+	gpio_init( _a_gpio );
+    gpio_set_dir( _a_gpio, GPIO_IN );
+	gpio_disable_pulls( _a_gpio );
+	gpio_set_input_hysteresis_enabled( _a_gpio, true );
 
-	gpio_init( this->b_gpio );
-    gpio_set_dir( this->b_gpio, GPIO_IN );
-	gpio_disable_pulls( this->b_gpio );
-	gpio_set_input_hysteresis_enabled( this->b_gpio, true );
+	gpio_init( _b_gpio );
+    gpio_set_dir( _b_gpio, GPIO_IN );
+	gpio_disable_pulls( _b_gpio );
+	gpio_set_input_hysteresis_enabled( _b_gpio, true );
 
-	gpio_init( this->s_gpio );
-    gpio_set_dir( this->s_gpio, GPIO_IN );
-	gpio_disable_pulls( this->s_gpio );
-	gpio_set_input_hysteresis_enabled( this->s_gpio, true );
+	gpio_init( _s_gpio );
+    gpio_set_dir( _s_gpio, GPIO_IN );
+	gpio_disable_pulls( _s_gpio );
+	gpio_set_input_hysteresis_enabled( _s_gpio, true );
 
 	this->interrupts_init();
 }
@@ -76,37 +76,37 @@ void rotary_encoder::io_init() {
 
 void rotary_encoder::interrupts_init() {
 	gpio_irq_vtable_init();
-	gpio_irq_vtable_set_fn_callback( this->a_gpio, [this]( uint gpio, uint32_t events ) -> void { this->gpio_interrupt_a( gpio, events ); } );
-	gpio_irq_vtable_set_fn_callback( this->s_gpio, [this]( uint gpio, uint32_t events ) -> void { this->gpio_interrupt_s( gpio, events ); } );
+	gpio_irq_vtable_set_fn_callback( _a_gpio, [this]( uint gpio, uint32_t events ) -> void { this->gpio_interrupt_a( gpio, events ); } );
+	gpio_irq_vtable_set_fn_callback( _s_gpio, [this]( uint gpio, uint32_t events ) -> void { this->gpio_interrupt_s( gpio, events ); } );
 
-	gpio_set_irq_enabled( this->a_gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true );
-	gpio_set_irq_enabled( this->s_gpio, GPIO_IRQ_EDGE_FALL, true );
+	gpio_set_irq_enabled( _a_gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true );
+	gpio_set_irq_enabled( _s_gpio, GPIO_IRQ_EDGE_FALL, true );
     irq_set_enabled( IO_IRQ_BANK0, true );
 }
 
 //----------------------------------------------------------------
 
 void rotary_encoder::io_deinit() {
-	gpio_set_irq_enabled( this->a_gpio, 0, false );
-	gpio_set_irq_enabled( this->s_gpio, 0, false );
+	gpio_set_irq_enabled( _a_gpio, 0, false );
+	gpio_set_irq_enabled( _s_gpio, 0, false );
 
-	gpio_irq_vtable_set_callback( this->a_gpio, nullptr );
-	gpio_irq_vtable_set_callback( this->s_gpio, nullptr );
+	gpio_irq_vtable_set_callback( _a_gpio, nullptr );
+	gpio_irq_vtable_set_callback( _s_gpio, nullptr );
 
-	gpio_deinit( this->a_gpio );
-	gpio_deinit( this->b_gpio );
-	gpio_deinit( this->s_gpio );
+	gpio_deinit( _a_gpio );
+	gpio_deinit( _b_gpio );
+	gpio_deinit( _s_gpio );
 }
 
 //----------------------------------------------------------------
 
 int8_t rotary_encoder::use_rotation() {
-	if ( this->rotations > 0 ) {
-		this->rotations -= 1;
+	if ( _rotations > 0 ) {
+		_rotations -= 1;
 		return 1;
 	}
-	else if ( this->rotations < 0 ) {
-		this->rotations += 1;
+	else if ( _rotations < 0 ) {
+		_rotations += 1;
 		return -1;
 	}
 
@@ -116,14 +116,14 @@ int8_t rotary_encoder::use_rotation() {
 //----------------------------------------------------------------
 
 int8_t rotary_encoder::get_direction() const {
-	return (this->rotations > 0) ? +1 : ((this->rotations < 0) ? -1 : 0);
+	return (_rotations > 0) ? +1 : ((_rotations < 0) ? -1 : 0);
 }
 
 //----------------------------------------------------------------
 
 bool rotary_encoder::use_click() {
-	if ( this->clicks > 0 ) {
-		this->clicks -= 1;
+	if ( _clicks > 0 ) {
+		_clicks -= 1;
 		return true;
 	}
 
@@ -134,10 +134,10 @@ bool rotary_encoder::use_click() {
 
 void rotary_encoder::gpio_interrupt_a( uint gpio, uint32_t events ) {
 	if ( this->is_a_equal_b() ) {
-		this->rotations += 1;
+		_rotations += 1;
 	}
 	else {
-		this->rotations -= 1;
+		_rotations -= 1;
 	}
 }
 
@@ -145,7 +145,7 @@ void rotary_encoder::gpio_interrupt_a( uint gpio, uint32_t events ) {
 
 void rotary_encoder::gpio_interrupt_s( uint gpio, uint32_t events ) {
 	if ( this->get_s() == 0 ) {
-		this->clicks += 1;
+		_clicks += 1;
 	}
 }
 
--- rotary_encoder/rotary_encoder.h
+++ rotary_encoder/rotary_encoder.h
@@ -35,49 +35,57 @@ using rotary_encoder_ref = rotary_encoder*;
 //----------------------------------------------------------------
 
 class rotary_encoder {
-private:
-	uint a_gpio;
-	uint b_gpio;
-	uint s_gpio;
-
-	int32_t rotations;
-	uint32_t clicks;
 
 private:
+
+	uint _a_gpio;
+	uint _b_gpio;
+	uint _s_gpio;
+
+	int32_t _rotations;
+	uint32_t _clicks;
+
+private:
+
 	rotary_encoder();
 
 public:
+
 	rotary_encoder( uint a_gpio, uint b_gpio, uint s_gpio );
 	~rotary_encoder();
 
 private:
+
 	void io_init();
 	void interrupts_init();
 	void io_deinit();
 
 public:
-	inline uint get_a_gpio() const { return this->a_gpio; };
-	inline uint get_b_gpio() const { return this->b_gpio; };
-	inline uint get_s_gpio() const { return this->s_gpio; };
 
-	inline bool get_a() const { return gpio_get( this->a_gpio ); };
-	inline bool get_b() const { return gpio_get( this->b_gpio ); };
-	inline bool get_s() const { return gpio_get( this->s_gpio ); };
+	inline uint get_a_gpio() const { return _a_gpio; };
+	inline uint get_b_gpio() const { return _b_gpio; };
+	inline uint get_s_gpio() const { return _s_gpio; };
+
+	inline bool get_a() const { return gpio_get( _a_gpio ); };
+	inline bool get_b() const { return gpio_get( _b_gpio ); };
+	inline bool get_s() const { return gpio_get( _s_gpio ); };
 
 	inline bool is_a_equal_b() const { return this->get_a() == this->get_b(); };
 	inline bool is_a_notequal_b() const { return this->get_a() != this->get_b(); };
 
-	inline int32_t get_rotations() const { return this->rotations; };
+	inline int32_t get_rotations() const { return _rotations; };
 	int8_t use_rotation();
 
 	int8_t get_direction() const;
 
-	inline uint32_t get_clicks() const { return this->clicks; };
+	inline uint32_t get_clicks() const { return _clicks; };
 	bool use_click();
 
 private:
+
 	void gpio_interrupt_a( uint gpio, uint32_t events );
 	void gpio_interrupt_s( uint gpio, uint32_t events );
+
 };
 
 //----------------------------------------------------------------
