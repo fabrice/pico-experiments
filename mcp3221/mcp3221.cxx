c976e6081d117ea9ceb4ee7d019ca14fd74bb4b4
fabrice truillot de chambrier <fabrice@dechambrier.net>
2026-05-08 00:48:39 +0200

    mcp3221 added

---
+++ mcp3221/mcp3221.cxx
@@ -0,0 +1,65 @@
+//----------------------------------------------------------------
+//
+// Raspberry Pico
+// Target : PicoSDK C/C++
+// CFPT Électronique
+//
+// mcp3221
+//
+//----------------------------------------------------------------
+
+#include "mcp3221.h"
+
+#include "pico/stdlib.h"
+#include "hardware_i2c_plus.h"
+
+//----------------------------------------------------------------
+
+mcp3221* mcp3221::make( uint i2c_num, uint8_t address ) {
+	auto wire = wire_i2c::make( i2c_num, address );
+	auto that = new mcp3221( wire );
+
+	return that;
+}
+
+//----------------------------------------------------------------
+
+mcp3221::mcp3221( wire* wire ):
+		_wire { wire } {
+}
+
+//----------------------------------------------------------------
+
+mcp3221::~mcp3221() {
+	if ( _wire != nullptr ) {
+		delete _wire;
+		_wire = nullptr;
+	}
+}
+
+//----------------------------------------------------------------
+
+void mcp3221::start_adc() {
+	if ( _wire == nullptr ) return;
+
+	_wire->start_communication();
+	_wire->write_bytes( 0 );
+	_wire->finish_communication();
+}
+
+//----------------------------------------------------------------
+
+uint16_t mcp3221::read_adc() {
+	if ( _wire == nullptr ) return 0;
+
+	_wire->start_transaction();
+	size_t rlength = 2;
+	uint8_t data[2] { 0 };
+	int result = _wire->read_bytes( data, &rlength );
+	_wire->finish_transaction();
+	_adc = (static_cast< uint16_t >( data[0] ) << 8) | (static_cast< uint16_t >( data[1] ) << 0);
+
+	return _adc;
+}
+
+//----------------------------------------------------------------
---
+++ mcp3221/mcp3221.h
@@ -0,0 +1,52 @@
+//----------------------------------------------------------------
+//
+// Raspberry Pico
+// Target : PicoSDK C/C++
+// CFPT Électronique
+//
+// adc
+// mcp3221
+//
+//----------------------------------------------------------------
+
+#pragma once
+
+//----------------------------------------------------------------
+
+#include "pico/stdlib.h"
+
+#include "wire_i2c.h"
+#include "hardware_i2c_plus.h"
+
+//----------------------------------------------------------------
+
+constexpr uint8_t MCP3221_ADDRESS { 0x4d }; // 0x9a >> 1
+
+//----------------------------------------------------------------
+
+class mcp3221 {
+
+private:
+
+	wire* _wire { nullptr };
+	uint8_t _address { MCP3221_ADDRESS };
+
+	uint16_t _adc { 0 };
+
+public:
+
+	static mcp3221* make( uint i2c_num, uint8_t address );
+
+	mcp3221() = delete;
+	mcp3221( wire* wire );
+
+	~mcp3221();
+
+	uint16_t get_adc() { return _adc; }
+
+	void start_adc();
+	uint16_t read_adc();
+
+};
+
+//----------------------------------------------------------------
